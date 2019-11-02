6f6a6716a7dc52bfc062a3f4adcc8c6227814c5c
Jexulie <fejitj3n@yahoo.com>
2019-11-02 20:24:11 +0300

    init repo

---
+++ tests/tempus.test.js
@@ -0,0 +1,8 @@
+const tempus = require('../main.js');
+
+
+let f = new tempus(new Date()).strftime('mm/dd/yyyy ww HH:MM:SS.s');
+let s = new tempus("09-25-1997").strftime('dd.mm.yyyy ww HH:MM:SS.s');
+
+console.log(f);
+console.log(s);
